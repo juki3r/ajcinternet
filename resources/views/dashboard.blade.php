64f921957de0759b6607ab00f6505dacc829e40d
juki3r <deepong25@gmail.com>
2024-03-14 08:48:53 +0800

    modified:   resources/views/dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,15 +6,28 @@
     </x-slot>
 
     @if(Auth::user()->usertype==='user')
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ Auth::user()->usertype}}
+        @if(Auth::user()->name == 'Reman Edem')
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ Auth::user()->usertype}}
+                        {{ Auth::user()->name}}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+        @else 
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        {{ Auth::user()->usertype}}
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endif
 
     @else
 

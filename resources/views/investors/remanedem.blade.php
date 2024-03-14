6141750510f721bc9c66308ec330e7d7715cdd9c
juki3r <deepong25@gmail.com>
2024-03-14 10:23:08 +0800

    modified:   resources/views/investors/remanedem.blade.php

--- resources/views/investors/remanedem.blade.php
+++ resources/views/investors/remanedem.blade.php
@@ -49,6 +49,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
+                        <div class="row">
                         {{-- January --}}
                         <div class="col-12 col-lg-6 my-2">
                             <div class="card shadow">
@@ -87,6 +88,7 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
                     </div>
                 </div>
             </div>

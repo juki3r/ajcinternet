db826995e271bb55f2e4164895827ed57b5898b5
juki3r <deepong25@gmail.com>
2024-03-14 11:05:17 +0800

    modified:   resources/views/investors/remanedem.blade.php

--- resources/views/investors/remanedem.blade.php
+++ resources/views/investors/remanedem.blade.php
@@ -8,7 +8,7 @@
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900">
-                        <h1 class="fs-2 py-2">Hello {{ Auth::user()->name }}</h1>                 
+                        <h1 class="fs-2 py-2">Hello, {{ Auth::user()->name }}</h1>                 
                         <div class="card">
                             <div class="card-header">
                                 @if(session('status'))

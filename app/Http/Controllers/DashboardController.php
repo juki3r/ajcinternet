b984da3a1012f4e6c991c713007a62559e12cf8d
juki3r <deepong25@gmail.com>
2024-03-14 10:09:31 +0800

    modified:   app/Http/Controllers/DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends Controller
         $frommarchcustomer = IslaSur::where('march', 'like', 'yes')->get();
 
 
-         return view('investores.remanedemstatus', [
+         return view('investors.remanedemstatus', [
             'deduction' => 1500,
             'january' => count($fromjanuarycustomer)*200,
             'febuary' => count($fromfebuarycustomer)*200,

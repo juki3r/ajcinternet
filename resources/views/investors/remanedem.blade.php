3e1c41863eff9a3cc938fdab48de3babc950c992
juki3r <deepong25@gmail.com>
2024-03-14 10:18:34 +0800

    modified:   resources/views/investors/remanedem.blade.php 	deleted:    resources/views/investors/remanedemstatus.blade.php

--- resources/views/investors/remanedem.blade.php
+++ resources/views/investors/remanedem.blade.php
@@ -7,64 +7,67 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">
-                        <div class="row">
-                            <div class="col">
-                                <div class="card">
-                                    <div class="card-header">
-                                        @if(session('status'))
-                                            <div class="alert alert-success">{{session('status')}}</div>
-                                        @endif
-                                        <h4 class="my-2">
-                                            Gigante Sur, Carles - Clients
-                                        </h4>
-                                    </div>
-                                    <div class="card-body">
-                                        <div class="table-responsive-sm mt-2">
-                                            <table class="table table-striped table-bordered table-hover table-sm text-nowrap text-center bg-info" id="sortTable">
-                                                <thead >
-                                                    <tr>
-                                                        <th>Name</th>
-                                                        <th>Address</th>
-                                                        <th>Plan</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody >
-                                                    @foreach($islasurdata as $islasurdata)
-                                                      <tr>
-                                                        <td>{{ $islasurdata->name }}</td>
-                                                        <td>{{ $islasurdata->address }}</td>
-                                                        <td>{{ $islasurdata->plan }}</td>
-                                                      </tr>
-                                                    @endforeach
-                                                </tbody>
-                                            </table>
-                                           
-                                        </div>
-                                    </div>
+                    <div class="p-6 text-gray-900">                    
+                        <div class="card">
+                            <div class="card-header">
+                                @if(session('status'))
+                                    <div class="alert alert-success">{{session('status')}}</div>
+                                @endif
+                                <h4 class="my-2">
+                                    Gigante Sur, Carles - Clients
+                                </h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive-sm mt-2">
+                                    <table class="table table-striped table-bordered table-hover table-sm text-nowrap text-center bg-info" id="sortTable">
+                                        <thead >
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Address</th>
+                                                <th>Plan</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody >
+                                            @foreach($islasurdata as $islasurdata)
+                                                <tr>
+                                                <td>{{ $islasurdata->name }}</td>
+                                                <td>{{ $islasurdata->address }}</td>
+                                                <td>{{ $islasurdata->plan }}</td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                    </table> 
                                 </div>
-                                <div class="col-12 col-lg-6">
-                                    {{-- January --}}
-                                    <div class="card">
-                                        <div class="card-header">
-                                            <h1>January</h1>
-                                        </div>
-                                        <div class="card-body">
-                                            <p>
-                                                Total Income : &#8369; {{ $january }}.00
-                                            </p>
-                                            <p>
-                                                Deduction : &#8369; {{ $deduction }}.00
-                                            </p>
-                                            <p>
-                                                Payable by admin : &#8369; {{ $january - $deduction }}.00
-                                            </p>
-                                        </div>
-                                    </div>
+                            </div>
+                        </div>              
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900">
+                        <div class="col-12 col-lg-6">
+                            {{-- January --}}
+                            <div class="card">
+                                <div class="card-header">
+                                    <h1>January</h1>
+                                </div>
+                                <div class="card-body">
+                                    <p>
+                                        Total Income : &#8369; {{ $january }}.00
+                                    </p>
+                                    <p>
+                                        Deduction : &#8369; {{ $deduction }}.00
+                                    </p>
+                                    <p>
+                                        Payable by admin : &#8369; {{ $january - $deduction }}.00
+                                    </p>
                                 </div>
                             </div>
                         </div>
-                       
                     </div>
                 </div>
             </div>
